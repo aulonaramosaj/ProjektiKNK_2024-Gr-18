ea675cb37a4ef0a97df19e7331efdc5f0c26d972
aulonaramosaj <aulona.ramosaj@student.uni-pr.edu>
2024-04-06 21:36:08 +0200

    Initial version

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.projekti.projektiknk_2024gr18 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.projekti.projektiknk_2024gr18 to javafx.fxml;
+    exports com.projekti.projektiknk_2024gr18;
+}

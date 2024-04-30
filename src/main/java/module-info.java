e54b0056b16daeb234c0de928a6dba4897e3a867
aulonaramosaj <aulona.ramosaj@student.uni-pr.edu>
2024-04-30 23:16:06 +0200

    added SignUp

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,12 @@
 module com.projekti.projektiknk_2024gr18 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.projekti.projektiknk_2024gr18 to javafx.fxml;
     exports com.projekti.projektiknk_2024gr18;
+
+    exports App;
+    opens controller to javafx.fxml;
 }

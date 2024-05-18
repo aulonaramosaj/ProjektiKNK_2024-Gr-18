fcfe1bc6e4dbe692cdda09bd56091f55cd3bc685
Brela Shala <brela.shala@student.uni-pr.edu>
2024-05-18 12:46:19 +0200

    HelpController Added

---
+++ src/main/java/controller/HelpController.java
@@ -0,0 +1,21 @@
+package controller;
+
+import App.Navigator;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelpController {
+    @FXML
+    private Label ckaeshteSistemi;
+    @FXML
+    private Label ckaeshtesistemi1;
+    @FXML
+    private Label funksionimiSistemit;
+    @FXML
+    private Label funksionimiSistemit1;
+    @FXML
+    private void handleFaqjaKryesore (ActionEvent ae){
+        // Navigator.navigate(ae, Navigator.HOME);
+    }
+}

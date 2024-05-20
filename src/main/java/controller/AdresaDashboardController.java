4bbfe5069dc0c6f42b9770fdf43aae02b88faa56
armeniasadikaj <armeniasadikaj@gmail.com>
2024-05-20 12:48:32 +0200

    Added AdresaDashboard

---
+++ src/main/java/controller/AdresaDashboardController.java
@@ -0,0 +1,76 @@
+package controller;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public class AdresaDashboardController {
+        @FXML
+        private AnchorPane root;
+
+        @FXML
+        private ImageView imageView;
+
+        @FXML
+        private Label AdresaDashboard;
+
+        @FXML
+        private Label komunaLabel;
+
+        @FXML
+        private Label kodiPostarLabel;
+
+        @FXML
+        private Label fshatiLagjiaLabel;
+
+        @FXML
+        private Label rrugaLabel;
+
+        @FXML
+        private Label numriNdertesesLabel;
+
+        @FXML
+        private TextField komunaTextField;
+
+        @FXML
+        private TextField kodiPostarTextField;
+
+        @FXML
+        private TextField fshatiLagjiaTextField;
+
+        @FXML
+        private TextField rruga;
+
+        @FXML
+        private TextField NrNderteses;
+
+        @FXML
+        private TableView<?> tableView;
+
+        @FXML
+        private TableColumn<?, ?> idColumn;
+
+        @FXML
+        private TableColumn<?, ?> komunaColumn;
+
+        @FXML
+        private TableColumn<?, ?> kodiPostarColumn;
+
+        @FXML
+        private TableColumn<?, ?> fshatiLagjiaColumn;
+
+        @FXML
+        private TableColumn<?, ?> rrugaColumn;
+
+        @FXML
+        private TableColumn<?, ?> numriNdertesesColumn;
+        @FXML
+        private Button button;
+
+
+}
+
+
+
+
